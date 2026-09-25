611576c7ce4304b3decd55bf0bde9ae38952f0ba
Moloko-TG <rogovecserik@gmail.com>
2026-09-25 10:22:18 +0300

    neizpildits

---
+++ 01_bilesu_cena/main.py
@@ -0,0 +1,18 @@
+print("Biļešu cenas kalkulators")
+
+vecums = int(input("Ievadi savu vecumu: "))
+
+cena = 0
+
+if vecums <= 6:
+    cena = 0
+elif 7 <= vecums <= 17:
+    cena = 3
+elif 18 <= vecums <= 64:
+    cena = 7
+else:
+    cena = 4
+
+
+print(f"Biļetes cena ir {cena} EUR.")
+
